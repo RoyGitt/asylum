0d79791c7d85fec37ddc0e093df3b0f3ba484abb
RoyGitt <officialroy02@gmail.com>
2023-10-27 23:36:12 +0530

    Fix node modules uploaded on github

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
 
-app.listen(3000,()=>{
-    console.log("Server is running at PORT 3000")
-})
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MONGO");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.listen(3000, () => {
+  console.log("Server is running at PORT 3000");
+});
