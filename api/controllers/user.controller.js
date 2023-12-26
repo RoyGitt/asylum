4d9b2235699e74bef7b6596ff8f75428e9a6a512
RoyGitt <officialroy02@gmail.com>
2023-12-27 02:48:55 +0530

    Create update user route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,39 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcrypt from "bcrypt";
+
 export const test = (req, res) => {
   res.json({ message: "Test" });
+};
+
+export const updateUser = async (req, res, next) => {
+  const user = req.user;
+
+  if (user.id !== req.params.id) {
+    return next(errorHandler(401, "You can only update your own account"));
+  }
+
+  try {
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...modifiedUser } = updatedUser;
+
+    res.status(200).json(modifiedUser);
+  } catch (err) {
+    next(err);
+  }
 };
