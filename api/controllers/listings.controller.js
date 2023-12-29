6c99f75958d5476383f9e559c0087c511470a4c5
RoyGitt <officialroy02@gmail.com>
2023-12-29 17:16:07 +0530

    Create delete user route

--- api/controllers/listings.controller.js
+++ api/controllers/listings.controller.js
@@ -1,9 +1,22 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
     res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    next(errorHandler("401", "You can only delete your own listings"));
+  }
+  try {
+    await Listing.findOneAndDelete({ userRef: req.params.id });
+    res.json({ message: "Deleted Successfully" });
   } catch (error) {
     next(error);
   }
--- api/routes/listings.route.js
+++ api/routes/listings.route.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { createListing } from "../controllers/listings.controller.js";
+import {
+  createListing,
+  deleteListing,
+} from "../controllers/listings.controller.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/create-listing", createListing);
+router.delete("/delete-listing/:id", verifyToken, deleteListing);
 
 export default router;
