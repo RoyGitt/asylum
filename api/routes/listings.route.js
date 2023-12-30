b403b9e03422be93fdad3921a9714a19cc45cafc
RoyGitt <officialroy02@gmail.com>
2023-12-30 16:44:08 +0530

    Create search route

--- api/routes/listings.route.js
+++ api/routes/listings.route.js
@@ -4,6 +4,7 @@ import {
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 } from "../controllers/listings.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
@@ -13,5 +14,6 @@ router.post("/create-listing", createListing);
 router.delete("/delete-listing/:id", verifyToken, deleteListing);
 router.post("/update-listing/:id", verifyToken, updateListing);
 router.get("/get-listing/:id", getListing);
+router.get("/get-listings", getListings);
 
 export default router;
