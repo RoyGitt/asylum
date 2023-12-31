fad9bc4c4eea5793f9d6e7b46b0d911000bf2f95
RoyGitt <officialroy02@gmail.com>
2023-12-31 22:14:20 +0530

    Add pre installation

--- api/index.js
+++ api/index.js
@@ -9,9 +9,13 @@ import listingsRouter from "./routes/listings.route.js";
 
 import cookieParser from "cookie-parser";
 
+import path from "path";
+
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
 
 mongoose
   .connect(process.env.MONGO)
@@ -33,9 +37,16 @@ app.listen(3000, () => {
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listings", listingsRouter);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
